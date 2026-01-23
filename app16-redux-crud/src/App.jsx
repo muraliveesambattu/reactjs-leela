c1637aadf3478377f4a006e3a04dc692303f15d6
vmpavanireddy <vmpavanireddy@gmail.com>
2026-01-23 15:29:28 +0530

    Added CRUD

--- app16-redux-crud/src/App.jsx
+++ app16-redux-crud/src/App.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import './App.css'
-import { useEffect } from 'react';
-import { getAllUsersAsyncAction } from './store/userSlice';
+import { useEffect, useState } from 'react';
+import { createUsersAsyncAction, deleteUsersAsyncAction, getAllUsersAsyncAction } from './store/userSlice';
 
 function App() {
   // const {users} = useSelector((state)=>{
@@ -9,18 +8,121 @@ function App() {
   // })
   const dispatch = useDispatch();
   const { users } = useSelector((store) => store.usersDetails);
-
+  const [isEdit, setIsEdit] = useState(false)
+  const [user, setUser] = useState({
+    id: "",
+    fname: "",
+    lname: "",
+    email: "",
+    state: "",
+    zip: "",
+  });
   console.log(users);
   useEffect(() => {
     dispatch(getAllUsersAsyncAction())
   }, [])
+
+  const handleDelete = (user) => {
+    dispatch(deleteUsersAsyncAction(user));
+  }
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted User:", user);
+    dispatch(createUsersAsyncAction(user))
+    clearForm()
+  };
+  const clearForm = () => {
+    setUser({
+      id: "",
+      fname: "",
+      lname: "",
+      email: "",
+      state: "",
+      zip: "",
+    })
+  }
+  const handleEdit = (user) => {
+    setUser(user);
+    setIsEdit(true)
+  }
   return (
     <>
       <h2>React Redux CRUD Demo</h2>
+      <button style={{ background: "blue", color: "white" }}>Create User</button>
       <ul>
-        {users.map((user)=> <li key={user.id}>
-{user.email}
-        </li> )}
+        {users.map((user) => <li key={user.id}>
+          {user.email} - <button onClick={() => { handleDelete(user) }} style={{ background: 'red', color: 'white', margin: '5px' }}>Delete User</button> - <button onClick={() => { handleEdit(user) }} style={{ background: 'yellow', color: 'blue', margin: '5px' }}>Edit User</button>
+        </li>)}
+
+        <br />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            name="id"
+            placeholder="Enter ID"
+            value={user.id}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+
+          <input
+            type="text"
+            name="fname"
+            placeholder="Enter First Name"
+            value={user.fname}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+
+          <input
+            type="text"
+            name="lname"
+            placeholder="Enter Last Name"
+            value={user.lname}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            value={user.email}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+
+          <input
+            type="text"
+            name="state"
+            placeholder="Enter State (eg: TN)"
+            value={user.state}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+
+          <input
+            type="text"
+            name="zip"
+            placeholder="Enter ZIP Code"
+            value={user.zip}
+            onChange={handleChange}
+            required
+          />
+          <br /><br />
+          {isEdit ? <button type="submit" style={{ background: 'green', color: "white" }}>Update</button> : <button type="submit" style={{ background: 'green', color: "white" }}>Submit</button>
+
+          }
+        </form>
       </ul>
     </>
   )
--- app16-redux-crud/src/store/userSlice.js
+++ app16-redux-crud/src/store/userSlice.js
@@ -2,26 +2,47 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const initialState = {
     users: []
 }
-const API_URL = "http://localhost:3000/users"
-export const createUsersAsyncAction = createAsyncThunk('users/getAllUsersAsync', () => { });
+const API_URL = "http://localhost:3000/users/"
+export const createUsersAsyncAction = createAsyncThunk('users/getAllUsersAsync', async (user) => {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    return await response.json(); // return created user
+
+
+
+});
 
 export const getAllUsersAsyncAction = createAsyncThunk('users/createUserAsync', async () => {
-    console.log(
-        "createUserAsyncAction Called !!!"
-    );
     const response = await fetch(API_URL);
     return response.json()
 });
 export const updateUsersAsyncAction = createAsyncThunk('users/updateUserAsync', () => { });
-export const deleteUsersAsyncAction = createAsyncThunk('users/deleteUserAsync', () => { });
+
+export const deleteUsersAsyncAction = createAsyncThunk('users/deleteUserAsync', async (user) => {
+    const response = await fetch(API_URL + user.id, {
+        method: "DELETE"
+    })
+    return user.id
+});
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: initialState,
     extraReducers: (builder) => {
-        builder.addCase(getAllUsersAsyncAction.fulfilled,((state,action)=>{
+        builder.addCase(getAllUsersAsyncAction.fulfilled, ((state, action) => {
             state.users = action.payload
         }))
+        builder.addCase(deleteUsersAsyncAction.fulfilled, (state, action) => {
+            state.users = state.users.filter((usr) => usr.id !== action.payload)
+        })
+        builder.addCase(createUsersAsyncAction.fulfilled, (state, action) => {
+            state.users.push(action.payload);
+        })
     }
 
 })
